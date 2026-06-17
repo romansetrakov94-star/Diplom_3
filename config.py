577856fb593989326f3cf7634a0163eb775f0d8c
Roman Setrakov <roman.setrakov94@gmail.com>
2026-06-17 20:32:13 +0300

    Init main

--- config.py
+++
@@ -1,5 +0,0 @@
-class Config:
-    BASE_URL = "https://stellarburgers.education-services.ru"
-    LOGIN_URL = f"{BASE_URL}/login"
-    FEED_URL = f"{BASE_URL}/feed"
-    
--- locators/login_page_locators.py
+++
@@ -1,7 +0,0 @@
-from selenium.webdriver.common.by import By
-
-
-class LoginPageLocators:
-    EMAIL_FIELD = (By.XPATH, "//input[@name='name']")
-    PASSWORD_FIELD = (By.XPATH, "//input[@name='Пароль']")
-    LOGIN_BUTTON = (By.XPATH, "//button[text()='Войти']")
